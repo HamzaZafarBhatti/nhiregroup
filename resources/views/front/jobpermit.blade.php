72834c4a87f83e998a7704008ff133521c20c90e
Hamza Zafar Bhatti <49752653+HamzaZafarBhatti@users.noreply.github.com>
2023-07-18 05:45:40 +0500

    Update jobpermit.blade.php

--- resources/views/front/jobpermit.blade.php
+++ resources/views/front/jobpermit.blade.php
@@ -6,13 +6,14 @@
             margin-top: 2rem;
         }
 
-        .code-result h5 {
+        .code-result .valid {
             color: green;
             font-weight: bold;
         }
 
-        .code-result.invalid h5 span {
+        .code-result .invalid {
             color: red;
+            font-weight: bold;
         }
     </style>
 @endsection
@@ -40,18 +41,18 @@
                         <button class="btn btn-primary" type="submit">Authenticate Code</button>
                     </form>
                     @if (!empty($code))
-                        <div class="code-result {{ $code->is_purchased ? 'invalid' : 'valid' }}">
-                            <h5>Status of Job Permit: <span>{{ $code->is_purchased ? 'INVALID' : 'VALID' }}</span>
-                            </h5>
+                        <div class="code-result">
                             @if ($code->is_purchased)
-                                <h5>The JOB PERMIT is <span>INVALID</span> and cannot be used to register on NHIRE</h5>
+                                <h5 class="invalid">Jobpass already been used. Jobpass can't be used to secure any job on
+                                    NHIRE</h5>
+                                <h5>Jobpass is used by <b>{{ $code->used_by->username }}</b></h5>
                             @else
-                                <h5>The JOB PERMIT is VALID and can be used to register on NHIRE</h5>
-                                <h4 class="text-underline">Expiry Date</h4>
-                                <h5>JOB PERMIT TYPE: {{ $code->package->name }}</h5>
-                                <h5>JOB PERMIT will expire in {{ $code->package->expiry_time }} months</h5>
-                                <h5>AGENT NAME: {{ $code->user->name }}</h5>
+                                <h5 class="valid">Status of Jobpass is valid and can be used to secure jobs on NHIRE</h5>
                             @endif
+                            <h4 class="text-underline">Expiry Date</h4>
+                            <h5>JOB PERMIT TYPE: {{ $code->package->name }}</h5>
+                            <h5>JOB PERMIT will expire in {{ $code->package->expiry_time }} months</h5>
+                            <h5>AGENT NAME: {{ $code->user->name }}</h5>
                         </div>
                     @endif
                 </div>
