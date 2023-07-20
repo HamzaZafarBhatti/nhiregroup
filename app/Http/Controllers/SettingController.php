b0c87b64e7f8ac1d7b84284c70770e598c491f35
Hamza Zafar Bhatti <49752653+HamzaZafarBhatti@users.noreply.github.com>
2023-07-20 23:25:10 +0500

    Update SettingController.php

--- app/Http/Controllers/SettingController.php
+++ app/Http/Controllers/SettingController.php
@@ -45,6 +45,7 @@ class SettingController extends Controller
             if ($image = $request->file('site_logo')) {
                 $filename = 'logo_' . time() . '.' . $image->getClientOriginalExtension();
                 $location =  $setting->getLogoPath() . $filename;
+                return $location;
                 move_uploaded_file($request->site_logo, $location);
                 $data['site_logo'] = $filename;
                 if ($setting->site_logo && file_exists($setting->getLogoPath() . $setting->site_logo)) {
